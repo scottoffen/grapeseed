ee1e2311b9e7199d2139c10e6d4f4e77fb1926fd
Scott Offen <3513626+scottoffen@users.noreply.github.com>
2023-10-23 16:05:04 -0600

    Add RouteTemplate and unit tests (#9)

---
+++ src/Grapeseed.Tests/RouteTemplateTests.cs
@@ -0,0 +1,103 @@
+using Grapevine.Exceptions;
+
+namespace Grapeseed.Tests;
+
+public class RouteTemplateTests
+{
+    [Theory]
+    [InlineData("", @"^.*$")]
+    [InlineData("^[a-z]$", "^[a-z]$")]
+    [InlineData("/api/resource/id", "(?i)^/api/resource/id$")]
+    [InlineData("/api/resource/{id}", "(?i)^/api/resource/([^/]+)$")]
+    [InlineData("/api/resource/{id}/list", "(?i)^/api/resource/([^/]+)/list$")]
+    [InlineData("/api/resource/{id}/{value}", "(?i)^/api/resource/([^/]+)/([^/]+)$")]
+    [InlineData("/api/resource/{id:length(10)}", @"(?i)^/api/resource/([^/]{10})$")]
+    [InlineData("/api/resource/{id:length(1,10)}", @"(?i)^/api/resource/([^/]{1,10})$")]
+    [InlineData("/api/resource/{id:max(10)}", @"(?i)^/api/resource/([^/]{,10})$")]
+    [InlineData("/api/resource/{id:maxlength(10)}", @"(?i)^/api/resource/([^/]{,10})$")]
+    [InlineData("/api/resource/{id:min(10)}", @"(?i)^/api/resource/([^/]{10,})$")]
+    [InlineData("/api/resource/{id:minlength(10)}", @"(?i)^/api/resource/([^/]{10,})$")]
+    [InlineData("/api/resource/{id:numeric:length(10)}", @"(?i)^/api/resource/(\d{10})$")]
+    [InlineData("/api/resource/{id:num:length(10)}", @"(?i)^/api/resource/(\d{10})$")]
+    [InlineData("/api/resource/{id:num:length(1,10)}", @"(?i)^/api/resource/(\d{1,10})$")]
+    [InlineData("/api/resource/{id:num:length(10,1)}", @"(?i)^/api/resource/(\d{1,10})$")]
+    [InlineData("/api/resource/{id:num:max(10)}", @"(?i)^/api/resource/(\d{,10})$")]
+    [InlineData("/api/resource/{id:num:maxlength(10)}", @"(?i)^/api/resource/(\d{,10})$")]
+    [InlineData("/api/resource/{id:num:min(10)}", @"(?i)^/api/resource/(\d{10,})$")]
+    [InlineData("/api/resource/{id:num:minlength(10)}", @"(?i)^/api/resource/(\d{10,})$")]
+    public void ConvertsTemplateToRegexCorrectly(string pattern, string expected)
+    {
+        var actual = new RouteTemplate(pattern);
+        actual.Pattern.ToString().ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData("/segment/{param}", 2, 1)]
+    [InlineData("/{param}/segment", 2, 1)]
+    [InlineData("/segment/{param1}/{param2}/segment", 4, 2)]
+    [InlineData("/segment/{param1}/segment/{param2}", 4, 2)]
+    [InlineData("{param1}/{param2}/segment/segment", 4, 2)]
+    [InlineData("/{param1}/segment/{param2}/segment", 4, 2)]
+    [InlineData("/{param1}", 1, 1)]
+    [InlineData("/segment", 1, 0)]
+    [InlineData("/there/are/no/parameters/here", 5, 0)]
+    [InlineData("^regex", 0, 0)]
+    public void IdentifiesSegmentsCorrectly(string pattern, int segments, int keys)
+    {
+        var actual = new RouteTemplate(pattern);
+        actual.Segments.ShouldBe(segments);
+        actual.Parameters.ShouldBe(keys);
+    }
+
+    [Fact]
+    public void WhenDuplicateParametersKeysArePresent_ThrowsException()
+    {
+        Should.Throw<DuplicateRouteConstraintKey>(() =>
+        {
+            _ = new RouteTemplate("/segment/{param}/{param}");
+        });
+    }
+
+    [Fact]
+    public void WhenASegmentContainsMultipleConstraints_ThrowsException()
+    {
+        Should.Throw<InvalidRouteParameterException>(() =>
+        {
+            _ = new RouteTemplate("/segment/{param1}{param2}/");
+        });
+    }
+
+    [Theory]
+    [InlineData("/users/{id}", "/users/1234", true)]
+    [InlineData("/users/{id}", "/users/name", true)]
+    [InlineData("/users/{id:alpha}", "/users/1234", false)]
+    [InlineData("/users/{id:num}", "/users/1234", true)]
+    [InlineData("/users/{id:alpha}", "/users/name", true)]
+    [InlineData("/users/{id:num}", "/users/name", false)]
+    public void MatchesReturnCorrectValue(string pattern, string route, bool expected)
+    {
+        var template = new RouteTemplate(pattern);
+        var actual = template.Matches(route);
+        actual.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData("/segment/11aa22", true)]
+    [InlineData("/segment/aa11bb", false)]
+    public void UsingCustomResolver_MatchesReturnsCorrectValue(string route, bool expected)
+    {
+        RouteConstraints.AddResolver("custom", CustomResolver);
+        var pattern = @"/segment/{id:custom}";
+
+        var template = new RouteTemplate(pattern);
+        template.Pattern.ToString().ShouldBe(@"(?i)^/segment/(\d{2}[a-z]{2}\d{2})$");
+
+        var actual = template.Matches(route);
+        actual.ShouldBe(expected);
+    }
+
+    private string CustomResolver(IEnumerable<string> args)
+    {
+        return @"(\d{2}[a-z]{2}\d{2})";
+    }
+}
---
+++ src/Grapeseed/RouteTemplate.cs
@@ -0,0 +1,154 @@
+using System.Text;
+using System.Text.RegularExpressions;
+using Grapevine.Exceptions;
+
+namespace Grapevine;
+
+/// <summary>
+/// The route template determines whether or not the request matches the pattern for the route and parses out any route parameters from the path.
+/// </summary>
+public interface IRouteTemplate
+{
+    /// <summary>
+    /// An enumeration of the parameter keys from the route pattern.
+    /// </summary>
+    /// <value></value>
+    HashSet<string> Keys { get; }
+
+    /// <summary>
+    /// Returns a boolean value indicating whether the specified endpoint matches the route pattern.
+    /// </summary>
+    /// <param name="endpoint"></param>
+    /// <returns></returns>
+    bool Matches(string endpoint);
+
+    /// <summary>
+    /// An integer representing the number of parameterized segments in the route pattern.
+    /// <remarks>A lower number means that the route template has a greater specificity.</remarks>
+    /// </summary>
+    /// <value></value>
+    int Parameters { get; }
+
+    /// <summary>
+    /// Returns a dictionary of values for each entry in Keys based on the regular expression Pattern.
+    /// </summary>
+    /// <param name="endpoint"></param>
+    /// <returns></returns>
+    IDictionary<string, string> Parse(string endpoint);
+
+    /// <summary>
+    /// Regular expression pattern for the route, automatically generated from the route path.
+    /// </summary>
+    /// <value></value>
+    Regex Pattern { get; }
+
+    /// <summary>
+    /// An integer representing the number segments in the route pattern
+    /// </summary>
+    /// <remarks>A higher number means that the route template has a greater specificity.</remarks>
+    /// <value></value>
+    int Segments { get; }
+}
+
+public partial class RouteTemplate : IRouteTemplate
+{
+    public HashSet<string> Keys { get; } = new HashSet<string>();
+
+    public int Parameters => Keys.Count;
+
+    public Regex Pattern { get; protected set; } = Default;
+
+    public int Segments { get; protected set; } = 0;
+
+    public RouteTemplate(string pattern)
+    {
+        ConvertTemplateToRegex(pattern);
+    }
+
+    public bool Matches(string endpoint) => Pattern.IsMatch(endpoint);
+
+    public IDictionary<string, string> Parse(string endpoint)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public partial class RouteTemplate
+{
+    private static readonly string CaseInsensitive = "(?i)";
+    private static readonly string RegexPrefix = $"{CaseInsensitive}^";
+    private static readonly string RegexSuffix = "$";
+
+    /// <summary>
+    /// The default regular expression.
+    /// </summary>
+    /// <remarks>Will be used if the provided pattern is null or empty.</remarks>
+    /// <returns></returns>
+    public static readonly Regex Default = new(@"^.*$");
+
+    /// <summary>
+    /// Converts the parameterized pattern to a regular expression.
+    /// </summary>
+    /// <param name="pattern"></param>
+    /// <returns></returns>
+    public void ConvertTemplateToRegex(string pattern)
+    {
+        // If no pattern is provided, the default pattern will be used.
+        if (string.IsNullOrWhiteSpace(pattern)) return;
+
+        // If provided pattern is a regular expression string,
+        // use the pattern as a regular expression
+        if (pattern.StartsWith("^"))
+        {
+            Pattern = new(pattern);
+            return;
+        }
+
+        // Get the number of route segments
+        var segments = pattern.SanitizePath().TrimStart('/').Split('/');
+        Segments = segments.Length;
+
+        // If the provided pattern contains no parameterized route
+        // constraints, use a regular expression with no matching groups
+        if (!pattern.Contains("{"))
+        {
+            Pattern = new($"{RegexPrefix}{pattern}{RegexSuffix}");
+            return;
+        }
+
+        // Otherwise, parse the pattern and create a regular expression
+        // for each parameterized route constraint
+        var builder = new StringBuilder(RegexPrefix);
+
+        segments.ToList().ForEach(segment =>
+        {
+            segment = segment.ToLower();
+            builder.Append('/');
+
+            // Segment is a parameterized route constraint
+            if (segment.StartsWith('{') && segment.EndsWith('}'))
+            {
+                var routeParameter = segment.TrimStart('{').TrimEnd('}');
+                if (routeParameter.Contains('{')) throw new InvalidRouteParameterException("A given route segment can only contain one route constraint");
+
+                var parameterSegments = routeParameter.Split(':');
+
+                var key = parameterSegments.First();
+                if (Keys.Contains(key)) throw new DuplicateRouteConstraintKey(key);
+
+                var constraint = RouteConstraints.Resolve(parameterSegments.Skip(1).ToList());
+
+                Keys.Add(key);
+                builder.Append(constraint);
+            }
+            // Segment is not a parameterized route constraint
+            else
+            {
+                builder.Append(segment);
+            }
+        });
+
+        builder.Append(RegexSuffix);
+        Pattern = new(builder.ToString());
+    }
+}
